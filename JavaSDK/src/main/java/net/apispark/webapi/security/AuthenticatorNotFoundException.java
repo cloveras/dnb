414aa15e56dd784629240993525322a5cd1aaf6a
Christian Løverås <cl@superelectric.net>
2017-09-06 12:07:02 +0200

    Removed stuff

--- JavaSDK/src/main/java/net/apispark/webapi/security/AuthenticatorNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package net.apispark.webapi.security;
-
-public class AuthenticatorNotFoundException extends RuntimeException {
-
-    public AuthenticatorNotFoundException(String message) {
-        super(message);
-    }
-}
--- NodeJS_SDK/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var APIdrafts = require('./sdks/aPIdrafts');
-var restletUtils = require('./restletUtils');
-
-module.exports = {
-  APIdrafts : APIdrafts,
-  restletUtils: restletUtils
-};
