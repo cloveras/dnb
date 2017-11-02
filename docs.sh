c42ecf557ad543a15d5861af33e51d79307cd184
Christian Løverås <cl@superelectric.net>
2017-11-02 13:12:47 +0100

    Git, Spectacle, Slate and scp

---
+++ docs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start the right place
+# ------------------------------------------------------------
+cd /Users/cl/Dropbox/DNB/API/dnb
+
+# Github
+# ------------------------------------------------------------
+git add 7580.json
+git commit -m "(Automatic push from documentation build)"
+git push
+
+# Spectacle
+# ------------------------------------------------------------
+spectacle 7580.json -t 7580
+scp -r /Users/cl/Dropbox/DNB/API/dnb/7580/* superelectric@login.domeneshop.no:www/tmp/7580/
+
+# Slate
+# ------------------------------------------------------------
+cd ../slate
+widdershins /Users/cl/Dropbox/DNB/API/dnb/7580.json -o source/index.html.md 
+bundle exec middleman build --clean
+scp -r scp -r /Users/cl/Dropbox/DNB/API/slate/build/* superelectric@login.domeneshop.no:www/tmp/7580.slate/
