95cd24c619d4dbf779f3ac78ef56f3d40a0e7b46
Christian Løverås <cl@superelectric.net>
2017-12-12 10:49:08 +0100

    Argument is now the Git commit message

--- docs.sh
+++ docs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+COMMITMESSAGE=$*
+
+echo "$COMMITMESSAGE"
+#exit 
+
 # Start the right place
 # ------------------------------------------------------------
 cd /Users/cl/Dropbox/DNB/API/dnb
@@ -7,18 +12,19 @@ cd /Users/cl/Dropbox/DNB/API/dnb
 # Github
 # ------------------------------------------------------------
 git add 7580.json
-git commit -m "(Automatic push from documentation build)"
+#git commit -m "(Automatic push from documentation build)"
+git commit -m "$COMMITMESSAGE"
 git push
 
 # Spectacle
 # ------------------------------------------------------------
 spectacle 7580.json -t 7580
-scp -r 7580/* superelectric@login.domeneshop.no:www/tmp/7580/
+scp -C -r 7580/* superelectric@login.domeneshop.no:www/tmp/7580/
 
 # Slate
 # ------------------------------------------------------------
 cd ../slate
 widdershins ../dnb/7580.json -o source/index.html.md 
 bundle exec middleman build --clean
-scp -r scp -r build/* superelectric@login.domeneshop.no:www/tmp/7580.slate/
+scp -C -r build/* superelectric@login.domeneshop.no:www/tmp/7580.slate/
 
