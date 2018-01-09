6e3d6c089daa22b3990aa598614c493990812345
Christian Løverås <cl@superelectric.net>
2018-01-09 17:03:52 +0100

    Included API drafts

--- docs.sh
+++ docs.sh
@@ -1,30 +1,45 @@
 #!/bin/bash
 
 COMMITMESSAGE=$*
+MAINDIR=/Users/cl/Dropbox/DNB/API/dnb
 
 echo "$COMMITMESSAGE"
-#exit 
+echo "$MAINDIR"
 
 # Start the right place
 # ------------------------------------------------------------
-cd /Users/cl/Dropbox/DNB/API/dnb
+cd $MAINDIR
 
 # Github
 # ------------------------------------------------------------
 git add 7580.json
-#git commit -m "(Automatic push from documentation build)"
 git commit -m "$COMMITMESSAGE"
 git push
 
+# 7580
+# ============================================================
+
 # Spectacle
-# ------------------------------------------------------------
 spectacle 7580.json -t 7580
 scp -C -r 7580/* superelectric@login.domeneshop.no:www/tmp/7580/
 
 # Slate
-# ------------------------------------------------------------
 cd ../slate
 widdershins ../dnb/7580.json -o source/index.html.md 
 bundle exec middleman build --clean
-scp -C -r build/* superelectric@login.domeneshop.no:www/tmp/7580.slate/
+scp -C -r build/* superelectric@login.domeneshop.no:www/tmp/7580.slate
 
+
+# API drafts
+# ============================================================
+cd $MAINDIR
+
+# Spectacle
+spectacle API.json -t dnb
+scp -C -r dnb/* superelectric@login.domeneshop.no:www/tmp/dnb/
+
+# Slate
+cd ../slate
+widdershins ../dnb/API.json -o source/index.html.md 
+bundle exec middleman build --clean
+scp -C -r build/* superelectric@login.domeneshop.no:www/tmp/dnb.slate
