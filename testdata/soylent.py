9f89f273389fc3f8745f7da5a2c6eb4b89898753
Christian Løverås <cl@superelectric.net>
2018-03-12 16:36:35 +0100

    Initial commit

--- testdata/soylent.py
+++ testdata/soylent.py
@@ -1,3 +1,42 @@
+#!/usr/local/bin/python3
+""" Quick hack to generate fake people and some data.
+
+TODO:
+- Valid Norwegian SSNs
+- Bank accounts: One or more per person
+- Debit cards: One or more per person
+- Credit cards: Zero or more per person
+- Transactions (this is the interesting part):
+    - Salary
+    - Mortgage (for some)
+    - Insurance (for some)
+    - Utilities (electricity, public fees, etc)
+    - Car related expenses (for some)
+    - Food, transport, other everyday transactions
+    - Various other purchases
+    - Ideally: In segments/groups, with patterns, etc (endless possibilities to make it "realistic")
+
+License:
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Christian Løverås"
+__contact__ = "cl@dnb.no"
+__copyright__ = "Copyright 2018, DNB Open Banking"
+__license__ = "GPLv3"
+__status__ = "Hack"
+__version__ = "0.0.1"
+
 import requests
 import json
 import random
@@ -5,63 +44,90 @@ from random import randrange
 from random import choice
 from faker import Faker
 
-fake = Faker('no_NO')
-person = ''
-numberOfPeople = 100
+def create_people (number_of_people):
+    '''
+    :param number_of_people: How many people should be created?
+    :return:
+    '''
 
-customerFileTxt = open('customers-generated.txt', 'a')
-customerFileJson = open('customers-generated.json', 'a')
+    fake = Faker('no_NO')
 
-url = 'https://dnbapistore.com/hackathon/customers/3.0/customer'
-apiKey = 'KEY_GOES_HERE'
-head = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + appKey}
+    # Person data for reuse in loop below
+    person = ''
+    day = ''
+    month = ''
+    year = ''
+    date_of_birth = ''
+    fn = ''
+    ln = ''
+    gender = ''
+    street = ''
+    postal_code = ''
+    city = ''
+    phone = ''
+    email = ''
+    customer_data = ''
 
-for i in range(numberOfPeople):
+    # Files to create, or append to
+    customerFileTxt = open('customers-generated.txt', 'a')
+    customerFileJson = open('customers-generated.json', 'a')
 
-    # Birth date, etc
-    day = randrange(1,25)
-    month = randrange(1,12)
-    year = randrange(1900,2018)
-    dateOfBirth = str(year) + '-' + str(month).zfill(2) + '-' + str(day).zfill(2) + 'T00:00:00.000Z'
-    pnr = str(randrange(10000,99999)).zfill(6) # Faking it. https://no.wikipedia.org/wiki/F%C3%B8dselsnummer
-    ssn = str(day).zfill(2) + str(month).zfill(2) + str(year)[-2] + pnr
+    # People of the world!
+    for i in range(number_of_people):
 
-    # Name
-    if random.choice((True,False)):
-        gender = 'Female'
-        fn = fake.first_name_female()
-    else:
-        gender = 'Male'
-        fn = fake.first_name_male()
-    ln = fake.last_name()
+        # Birth date, etc
+        day = randrange(1,25)
+        month = randrange(1,12)
+        year = randrange(1900,2018)
+        date_of_birth = str(year) + '-' + str(month).zfill(2) + '-' + str(day).zfill(2) + 'T00:00:00.000Z'
 
-    print(ssn + ' ' + fn + ' ' + ln)
+        # SSN: Faking it until Faker supports Norwegian SSNs: https://github.com/joke2k/faker/issues/714
+        pnr = str(randrange(10000,99999)).zfill(6)
+        ssn = str(day).zfill(2) + str(month).zfill(2) + str(year)[-2] + pnr
 
-    person = {
-        'personalNumber': ssn,
-        'firstName': fn,
-        'lastName': ln,
-        'dateOfBirth': dateOfBirth,
-        'gender': gender,
-        'nationality': 'Norwegian',
-        'address': {
-            'street': fake.street_name() + ' ' + fake.building_number(),
-            'postalCode': fake.postcode(),
-            'city': fake.city(),
-            'country': 'NO'
-        },
-        'phoneNumber': fake.phone_number(),
-        'email': fake.safe_email(),
-        'idNumber': str(randrange(0,999999)).zfill(6),
-        'idType': 'passport'
-    }
-    data = json.dumps(person)
+        # Name and gender
+        if random.choice((True,False)):
+            gender = 'Female'
+            fn = fake.first_name_female()
+        else:
+            gender = 'Male'
+            fn = fake.first_name_male()
+        ln = fake.last_name()
 
-    try:
-        requests.post(url, data=data, headers=head)
-    except requests.exceptions.RequestException as e:
-        print(e)
-        sys.exit(1)
+        # Contact information
+        street = fake.street_name() + ' ' + fake.building_number()
+        postal_code = fake.postcode()
+        city = fake.city()
+        phone = fake.phone_number()
+        email = fake.safe_email()
+        id_type = random.choice(('passport', 'driverslicense', 'nationalidcard'))
 
-    customerFileJson.write(data + '\n')
-    customerFileTxt.write(ssn + ' ' + fn + ' ' + ln + '\n')
+        # JSON
+        person = {
+            'personalNumber': ssn,
+            'firstName': fn,
+            'lastName': ln,
+            'dateOfBirth': date_of_birth,
+            'gender': gender,
+            'nationality': 'Norwegian',
+            'address': {
+                'street': street,
+                'postalCode': postal_code,
+                'city': city,
+                'country': 'NO'
+            },
+            'phoneNumber': phone,
+            'email': email,
+            'idType': id_type
+        }
+        data = json.dumps(person)
+
+        # Write to files in JSON and text format
+        customerFileJson.write(data + '\n')
+        customerFileTxt.write('%r, %r, %r, %r, %r, %r, %r, %r, %r, %r\n' % (ssn, fn, ln, gender, street, postal_code, city, phone, email, id_type))
+
+        # Vewry verbose
+        print('%r, %r, %r, %r, %r, %r, %r, %r, %r, %r' % (ssn, fn, ln, gender, street, postal_code, city, phone, email, id_type))
+
+# Business time
+create_people(100)
